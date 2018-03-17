a18974b29d5463a71403495e3f4cb424cfd9a8d7
Félix <someonefefe@gmail.com>
2018-03-17 14:57:43 +0100

    first reducer

--- pollen/src/messages/actions.js
+++ pollen/src/messages/actions.js
@@ -2,6 +2,7 @@
  * Created by Orion on 3/17/2018.
  */
 
+let messageIdCounter = 0;
 export const SEND_MESSAGE = 'SEND_MESSAGE';
 export const ADD_TAG = 'ADD_TAG';
 
@@ -9,8 +10,9 @@ export const sendMessage = ( message, author=null ) => {
     return async function( dispatch ){
         dispatch({
             type: SEND_MESSAGE,
+            id: messageIdCounter++,
             message: message,
-            author: author
+            author: author,
         });
     }
 };
--- pollen/src/messages/reducer.js
+++ pollen/src/messages/reducer.js
@@ -1,3 +1,16 @@
 /**
  * Created by Orion on 3/17/2018.
  */
+
+import { ADD_TAG, SEND_MESSAGE } from './actions';
+import { combineReducers } from 'redux'
+
+const byId = ( state = {}, action ) => {
+    switch( action.type ){
+        case SEND_MESSAGE:
+            return {
+                ...state,
+                [ action.id ] : { id: action.id, message: action.message, author: action.author}
+            }
+    }
+};
